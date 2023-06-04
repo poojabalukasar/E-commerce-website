cca2f6ede6da8a6381c0e72c34323555e0ca7e19
poojabalukasar <poojakasar100@gmail.com>
2023-06-04 13:06:28 +0530

    initial commit

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ErrorHandler = require("./middleware/error");
 const app = express();
+const path = require("path");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const user = require("./controller/user");
